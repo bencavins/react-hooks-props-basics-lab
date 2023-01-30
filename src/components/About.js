67cefc5accf18b0bcb24e74ec33c3a36a5c4045f
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-30 17:01:14 -0500

    do lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useDeferredValue } from "react";
+import user from "../data/user";
 
-function About() {
+import Links from "./Links";
+
+function About({bio}) {
+  // const pBio = <p>{bio}</p> ? bio : ""
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links
+        linkedin={user.links.linkedin}
+        github={user.links.github} />
     </div>
   );
 }
